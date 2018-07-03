2754e20ce0b793a78bf4fc58cd38e525463e7532
Alfie Darko <me@alfiedarko.co.uk>
2018-07-03 14:30:40 +0100

    GREEN: ReceiptPrinter > .getReceipt() >> Spend50 Discount > returns an array including the big spender discount

--- src/receiptPrinter.js
+++ src/receiptPrinter.js
@@ -8,11 +8,11 @@ class ReceiptPrinter {
     getReceipt(basket){
         let receipt = [] 
         const MUFFIN_DISCOUNT = "10% Muffin Discount!"
-        const BIG_SPEND_DISCOUNT = "Hey Big Spender! %5 Discount"
+        const BIG_SPEND_DISCOUNT = "Hey Big Spender! %5 Discount!"
+
         receipt.push(this.calculate.getLineTotals(basket))
-
         if (this.calculate.getMuffinDiscount(basket) > 0) receipt.push(MUFFIN_DISCOUNT)
-
+        if (this.calculate.getSpend50Discount(basket) > 50) receipt.push(BIG_SPEND_DISCOUNT)
 
         return receipt.reduce((acc, val) => acc.concat(val), [])
     }
--- test/receiptPrinter.test.js
+++ test/receiptPrinter.test.js
@@ -9,13 +9,24 @@ describe('ReceiptPrinter', () => {
     let mockLineTotals
     describe('.getReceipt()', () => {
         beforeEach(() => {
+            // calculateStub = sinon.createStubInstance(Calculate)
+            // mockLineTotals = sinon.stub(Calculate.prototype, 'getLineTotals').returns(["Cafe Latte: £4.75"])
+            // mockMuffinDiscount = sinon.stub(Calculate.prototype, 'getMuffinDiscount').returns(1)
+            // receiptPrinter = new ReceiptPrinter(new Calculate())
+        });
+        afterEach(() => {
+            mockLineTotals.restore()
+            mockMuffinDiscount.restore()
+        });
+
+        before(() => {
             calculateStub = sinon.createStubInstance(Calculate)
             mockLineTotals = sinon.stub(Calculate.prototype, 'getLineTotals').returns(["Cafe Latte: £4.75"])
             mockMuffinDiscount = sinon.stub(Calculate.prototype, 'getMuffinDiscount').returns(1)
-
             receiptPrinter = new ReceiptPrinter(new Calculate())
+
         });
-        afterEach(() => {
+        after(() => {
             mockLineTotals.restore()
             mockMuffinDiscount.restore()
         });
@@ -23,15 +34,41 @@ describe('ReceiptPrinter', () => {
             getBasket = ['Cafe Latte']
             expect(receiptPrinter.getReceipt(getBasket)).to.be.an('array')
         });
-
-        it('returns an array with the line totals', () => {
-            getBasket = ['Cafe Latte']
-            expect(receiptPrinter.getReceipt(getBasket)).to.be.an('array').that.includes("Cafe Latte: £4.75")
+        
+        describe('>> LineTotals', () => {
+            it('returns an array with the line totals', () => {
+                getBasket = ['Cafe Latte']
+                expect(receiptPrinter.getReceipt(getBasket)).to.be.an('array').that.includes("Cafe Latte: £4.75")
+            });
+        });
+        
+        describe('>> MuffinDiscount', () => {
+            it('returns an array including muffin discount if it is included', () => {
+                getBasket = ['Cafe Latte', 'Blueberry Muffin']
+                expect(receiptPrinter.getReceipt(getBasket)).to.be.an('array').that.includes("10% Muffin Discount!")
+            });
+    
         });
 
-        it('returns an array including muffin discount if it is included', () => {
-            getBasket = ['Cafe Latte', 'Blueberry Muffin']
-            expect(receiptPrinter.getReceipt(getBasket)).to.be.an('array').that.includes("10% Muffin Discount!")
+        describe('>> Spend50 Discount', () => {
+            before(() => {
+                calculateStub = sinon.createStubInstance(Calculate)
+                // mockLineTotals = sinon.stub(Calculate.prototype, 'getLineTotals').returns(["Cafe Latte: £4.75"])
+                mockSpend50Discount = sinon.stub(Calculate.prototype, 'getSpend50Discount').returns(51)
+                receiptPrinter = new ReceiptPrinter(new Calculate())
+    
+            });
+            it('returns an array including the big spender discount', () => {
+                getBasket = ['Cafe Latte', 'Cafe Latte','Cafe Latte','Cafe Latte',
+                'Cafe Latte','Cafe Latte','Cafe Latte','Cafe Latte','Cafe Latte',
+                'Cafe Latte','Cafe Latte','Cafe Latte','Cafe Latte','Cafe Latte',]
+                console.log(receiptPrinter.getReceipt(getBasket))
+                expect(receiptPrinter.getReceipt(getBasket)).to.be.an('array').that.includes("Hey Big Spender! %5 Discount!")
+            });
+            after(() => {
+                mockSpend50Discount.restore()
+            });
+    
         });
 
 
