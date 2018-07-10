35e89121afe0e0bb319f31010bc434f6f5415745
Alfie Darko <me@alfiedarko.co.uk>
2018-07-10 18:51:17 +0100

    GREEN: Front End Testing: Can add a single item to the cart

--- public/src/init.js
+++ public/src/init.js
@@ -1,12 +1,26 @@
 import Calculate from "./calculate";
-import Order from "./order"
-import Payments from "./payments"
-import ReceiptPrinter from "./receiptPrinter"
-import Till from "./till"
+import Order from "./order";
+import Payments from "./payments";
+import ReceiptPrinter from "./receiptPrinter";
+import Till from "./till";
+import Buttons from "./buttons";
+import click_events from "./click_events";
 
-const calculate = new Calculate()
-const order = new Order()
-const payments = new Payments()
-const receiptPrinter = new ReceiptPrinter(calculate)
+const calculate = new Calculate();
+const order = new Order();
+const payments = new Payments();
+const receiptPrinter = new ReceiptPrinter(calculate);
+const till = new Till(calculate, order, payments, receiptPrinter);
+const buttons = new Buttons(till);
 
-const till = new Till(calculate, order, payments, receiptPrinter)
+$(document).ready(function() {
+  $("#add-name-button").click(function() {
+    let customerNameInput = $("#customer-names-input").val();
+    buttons.addName(customerNameInput);
+  });
+
+  $("#add-to-cart-button").click(function() {
+    buttons.addToCart();
+  });
+});
+console.log("drogba");
--- public/src/till.js
+++ public/src/till.js
@@ -1,19 +1,26 @@
 class Till {
-    constructor(calculate, order, payments, receiptPrinter) {
-        this.calculate = calculate
-        this.order = order
-        this.payments = payments
-        this.receiptPrinter = receiptPrinter
-    }
+  constructor(calculate, order, payments, receiptPrinter) {
+    this.calculate = calculate;
+    this.order = order;
+    this.payments = payments;
+    this.receiptPrinter = receiptPrinter;
+  }
 
-    getCustomerName(){
-        return this.order.getCustomerName()
-    }
+  getCustomerName() {
+    return this.order.getCustomerName();
+  }
 
-    setCustomerName(name){
-        this.order.setCustomerName(name)
-    }
+  setCustomerName(name) {
+    this.order.setCustomerName(name);
+  }
+
+  setBasket(food) {
+    this.order.setBasket(food);
+  }
+
+  getBasket() {
+    return this.order.getBasket();
+  }
 }
 
-export default Till
-
+export default Till;
